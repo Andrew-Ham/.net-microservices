8d2826bf307fbd3d0da549c220a45969957234b2
Andrew Ham <yham023@aucklanduni.ac.nz>
2023-02-06 18:25:54 +1300

    Moved Dependency injection and Config to new class

--- src/Play.Catalog.Service/Program.cs
+++ src/Play.Catalog.Service/Program.cs
@@ -1,31 +1,13 @@
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Driver;
 using Play.Catalog.Service.Entities;
 using Play.Catalog.Service.Repositories;
 using Play.Catalog.Service.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure ServiceSetting
-var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-
-// AddSingleton allows us to register a type or an object and makes sure there is ONLY ONE instance of it in the entire microservice.
-builder.Services.AddSingleton(serviceProvider => {
-    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-    BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-    var database = mongoClient.GetDatabase(serviceSettings.ServiceName);
-    return database;
-});
-
-
-builder.Services.AddSingleton<IRepository<Item>>(serviceProvider => {
-    var database = serviceProvider.GetService<IMongoDatabase>();
-    return new MongoRepository<Item>(database, "items");
-});
+// Register Mongo client and IMongo database
+// Since this is fluent, we can say .AddMongoRepository<Item>("items");
+builder.Services.AddMongo()
+                .AddMongoRepository<Item>("items");
 
 
 builder.Services.AddControllers(options => {options.SuppressAsyncSuffixInActionNames = false;});
@@ -41,7 +23,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.UseHttpsRedirection(); 
 
